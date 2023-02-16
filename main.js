84503a32af30180789afd5c0fcf0a35f8c91a32a
KyomiTeam <77785840+KyomiTeam@users.noreply.github.com>
2023-02-16 19:12:10 +0100

    v

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const { app, BrowserWindow } = require('electron')
 const log = require('electron-log')
+
 log.transports.file.resolvePath = () => path.join('./', '/logs/main.log')
+log.log("Applcation Version: " + app.getVersion())
 
 const { autoUpdater } = require('electron-updater')
 
@@ -22,8 +24,9 @@ autoUpdater.on('update-available', () => {
 autoUpdater.on('checking-for-update', () => {
     log.info("checking-for-update")
 })
-autoUpdater.on('download-progress', () => {
+autoUpdater.on('download-progress', (progressTrack) => {
     log.info("download-progress")
+    log.info(progressTrack)
 })
 autoUpdater.on('update-downloaded', () => {
     log.info("update-downloaded")
